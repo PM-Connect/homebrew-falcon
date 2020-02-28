581efc33f4d4d4e8ece31ce62d1f152b642ccb7c
Marcus Speight <marcus@pmconnect.co.uk>
2020-02-28 14:28:41 +0000

    falcon-node 12.16.1

--- Formula/falcon-node.rb
+++ Formula/falcon-node.rb
@@ -2,7 +2,7 @@ class FalconNode < Formula
   desc "node.js dependency for falcon"
   homepage "https://falcon.pmcservices.co.uk"
   url ""
-  sha256 "32fad8538b554a240ba5f69b1aa2e1b051500e28af5635c149122c362b93e8ac"
+  sha256 "b5831b22ee97f6b5e67569746515748a60c91df8c1930e447eb5b085a4abaca1"
 
   def install
     share.install buildpath/"node"
